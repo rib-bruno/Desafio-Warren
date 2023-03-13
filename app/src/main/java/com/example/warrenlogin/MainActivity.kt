df9abf5327de0f548cad1a0a62e13fe7e5f1b7cb
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-03-13 18:49:55 -0300

    finalização da tela de login

--- app/src/main/java/com/example/warrenlogin/MainActivity.kt
+++ app/src/main/java/com/example/warrenlogin/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.warrenlogin
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import com.example.warrenlogin.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +28,52 @@ class MainActivity : AppCompatActivity() {
             val email = binding.editEmail.text.toString()
             val password = binding.editPassword.text.toString()
 
+            when {
+                email.isEmpty() -> {
+                    binding.editEmail.error = "Preencha o Email"
+                }
+                password.isEmpty() -> {
+                    binding.editPassword.error = "Preencha a Senha"
+                }
+
+                !email.contains("@warrenbrasil.com") -> {
+                    val snackbar = Snackbar.make(it, "E-mail inválido!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                password.length <= 5 -> {
+                    val snackbar = Snackbar.make(
+                        it,
+                        "A senha precisa ter pelo menos 6 caracteres!",
+                        Snackbar.LENGTH_SHORT
+                    )
+                    snackbar.show()
+                }
+                else -> {
+                    login(it)
+                }
+            }
+
         }
+    }
+
+    private fun login(view: View) {
+
+        val progressbar = binding.progressBar
+        progressbar.visibility = View.VISIBLE
+
+        binding.btnEntrar.isEnabled = false
+        binding.btnEntrar.setTextColor(Color.parseColor("#FFFFFF"))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateMainScreen()
+            val snackbar = Snackbar.make(view, "Login efetuado com sucesso", Snackbar.LENGTH_SHORT)
+            snackbar.show()
+        }, 3000)
+    }
+
+    private fun navigateMainScreen() {
+        val intent = Intent(this, MainScreen::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/warrenlogin/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.example.warrenlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+
+        supportActionBar!!.hide()
+    }
+}
